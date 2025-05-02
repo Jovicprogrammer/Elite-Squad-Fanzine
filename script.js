8f245ea45ee330b3c03189aef44e7123f0ae8ee4
Jovic_Penhaz <victosousa92@gmail.com>
2025-05-01 21:06:13 -0300

    versão inicial da navbar

---
+++ script.js
@@ -0,0 +1,12 @@
+
+const botaoModal = document.getElementById('venhaVer')
+const body = document.getElementById('body')
+
+botaoModal.addEventListener('click', () => {
+
+    const card = document.createElement('div')
+    card.classList.add('card-div')
+    card.innerHTML = "dfighsuhfifhoi dfoçisdfopaeoefiap adfpefiçgjspdog rosgiosrigp df igpsoigsg"
+    body.appendChild(card)    
+
+})
